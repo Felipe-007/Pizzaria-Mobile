42e4113cc1be02fec3e34f2d370821941e0c4db1
Felipe <felipe.presidencia@gmail.com>
2022-10-10 10:33:28 -0300

    C3 - Tela login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,34 @@
+//tela de login
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
 
-export default function SignIn(){
-  return(
-    <View>
-      <Text>Tela de login</Text>
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo.png')}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite o seu email"
+          placeholderTextColor="#F0F0F0"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Digite a sua senha"
+          placeholderTextColor="#F0F0F0"
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
