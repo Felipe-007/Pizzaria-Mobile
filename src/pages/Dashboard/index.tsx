dc30fbb2c115cc97b8628c39ddc534297848c2e0
Felipe <felipe.presidencia@gmail.com>
2022-10-12 10:41:13 -0300

    C9 - Abrindo a mesa

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import { StackPramsList } from '../../routes/app.routes';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export default function Dashboard(){
+
+  const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
+  const [number, setNumber] = useState('');
+
+  async function openOrder() {
+    if(number === ''){
+      return;
+    }
+
+    //precisa fazer a requisição e abrir a mesa e navegar para a proxima tela.
+    navigation.navigate('Order', { number: number, order_id: 'dc1917e8-351b-4efc-baf4-279b0c569341' })
+  }
 
   return(
     <SafeAreaView style={styles.container}>
@@ -13,9 +28,11 @@ export default function Dashboard(){
         placeholderTextColor="#F0F0F0"
         style={styles.input}
         keyboardType="numeric"
+        value={number}
+        onChangeText={setNumber}
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={openOrder}>
         <Text style={styles.buttonText}>Abrir mesa</Text>
       </TouchableOpacity>
     </SafeAreaView>
