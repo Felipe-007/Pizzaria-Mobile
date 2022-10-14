7ba141fef2befef4b1f47b4a2c5ce76e291fb9cb
Felipe <felipe.presidencia@gmail.com>
2022-10-14 10:29:11 -0300

    C11 - Tela order (pedido)

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import { styles } from './styles';
 import { useRoute, RouteProp } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+
 
 type RouteDetailParams = {
   Order: {
@@ -12,15 +14,35 @@ type RouteDetailParams = {
 
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
-export default function Order(){
+export default function Order() {
   const route = useRoute<OrderRouteProps>();
 
-  return(
-    <View style={styles.container}>
-      <Text>Tela order</Text>
-      <Text>
-        {route.params.number}
-      </Text>
+  return (
+    <View style={styles.container}>      
+      <View style={styles.header}>
+        <Text style={styles.title}>Mesa {route.params.number}</Text>
+        <TouchableOpacity>
+          <Feather name="trash-2" size={28} color="#FF3F4b" />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: '#FFF' }}>Pizza</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: '#FFF' }}>Pizza de calabresa</Text>
+      </TouchableOpacity>
+
+      <View style={styles.qtdContainer}>
+        <Text style={styles.qtdText}>Quantidade</Text>
+        <TextInput
+          style={[styles.input, { width: '60%', textAlign: 'center' }]}
+          placeholderTextColor="#F0F0F0"
+          keyboardType="numeric"
+          value="1"
+        />
+      </View>
     </View>
   )
 }
