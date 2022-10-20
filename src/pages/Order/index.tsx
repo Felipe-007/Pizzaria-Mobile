342a4d12f73a283b4f0691360c203f72f933d6e0
Felipe <felipe.presidencia@gmail.com>
2022-10-20 09:28:08 -0300

    C16 - Produtos de uma categoria

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -19,6 +19,11 @@ export type CategoryProps = {
   name: string;
 }
 
+type ProductProps = {
+  id: string;
+  name: string;
+}
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
@@ -26,10 +31,14 @@ export default function Order() {
   const navigation = useNavigation();
 
   const [category, setCategory] = useState<CategoryProps[] | []>([]);
-  const [categorySelected, setCategorySelected] = useState<CategoryProps>()
+  const [categorySelected, setCategorySelected] = useState<CategoryProps | undefined>()
   const [modalCategoryVisible, setModalCategoryVisible] = useState(false);
 
   const [amount, setAmount] = useState('1');
+
+  const [products, setProducts] = useState<ProductProps[] | []>([]);
+  const [productSelected, setProductSelected] = useState<ProductProps | undefined>()
+  const [modalProductVisible, setModalProductVisible] = useState(false);
 
   useEffect(() => {
     async function loadInfo() {
@@ -40,6 +49,21 @@ export default function Order() {
     }
     loadInfo();
   }, [])
+
+  useEffect(() => {
+    async function loadProducts() {
+     const response = await api.get('/category/product', {
+      params:{
+        category_id: categorySelected?.id
+      }
+     })
+
+     setProducts(response.data);
+     setProductSelected(response.data[0])
+    }
+
+    loadProducts();
+  }, [categorySelected])
 
   //excluir a order(mesa)
   async function handleCloseOrder() {
@@ -77,9 +101,13 @@ export default function Order() {
         </TouchableOpacity>
       )}
 
-      <TouchableOpacity style={styles.input}>
-        <Text style={{ color: '#FFF' }}>Pizza de calabresa</Text>
-      </TouchableOpacity>
+      {products.length !== 0 && (
+        <TouchableOpacity style={styles.input}>
+          <Text style={{ color: '#FFF' }}>
+            {productSelected?.name}
+          </Text>
+        </TouchableOpacity>
+      )}
 
       <View style={styles.qtdContainer}>
         <Text style={styles.qtdText}>Quantidade</Text>
